eb1f0c2e687c15cc757958190fe2f93088dfae48
paulaandrezza <paulaandrezza25@gmail.com>
2023-06-05 19:42:41 -0300

    add components folder

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar'
 import { Text, View } from 'react-native'
 
+import { ReminderCard } from '../components/ReminderCard'
+
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center">
       <Text className="font-title text-5xl text-zinc-50">Hello World!</Text>
+      <ReminderCard />
       <StatusBar style="light" translucent />
     </View>
   )
---
+++ components/ReminderCard.tsx
@@ -0,0 +1,5 @@
+import { Text } from 'react-native'
+
+export function ReminderCard() {
+  return <Text className="text-zinc-50">ReminderCard</Text>
+}
