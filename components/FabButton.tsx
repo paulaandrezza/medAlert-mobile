9f7cc7e95afd6e090bceea041a61fc9a17686ce6
paulaandrezza <paulaandrezza25@gmail.com>
2023-06-07 21:42:54 -0300

    add fabbutton

---
+++ components/FabButton.tsx
@@ -0,0 +1,13 @@
+import { AntDesign } from '@expo/vector-icons'
+import { TouchableOpacity } from 'react-native'
+
+export function FabButton() {
+  return (
+    <TouchableOpacity
+      activeOpacity={0.7}
+      className="absolute bottom-20 right-4 z-10 rounded-full bg-pink-400 p-4 "
+    >
+      <AntDesign name="plus" size={24} color="#f9fafb" />
+    </TouchableOpacity>
+  )
+}
