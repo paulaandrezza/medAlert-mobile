3b4e03859cca9c8ff2c13e9d6f44bd194ec7dc57
paulaandrezza <paulaandrezza25@gmail.com>
2023-06-08 19:26:41 -0300

    iniciando pagina medicacoes

--- app/screens/Medication.tsx
+++ app/screens/Medication.tsx
@@ -1,9 +1,19 @@
-import { Text, View } from 'react-native'
+import { ScrollView } from 'react-native'
+import { FabButton } from '../../components/FabButton'
+import { MedicationCard } from '../../components/MedicationCard'
 
 export default function Medication() {
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-gray-50">medication</Text>
-    </View>
+    <>
+      <ScrollView
+        className="mx-4"
+        contentContainerStyle={{ paddingBottom: 100 }}
+        showsVerticalScrollIndicator={false}
+      >
+        <MedicationCard />
+        <MedicationCard />
+      </ScrollView>
+      <FabButton />
+    </>
   )
 }
--- components/FabButton.tsx
+++ components/FabButton.tsx
@@ -5,7 +5,7 @@ export function FabButton() {
   return (
     <TouchableOpacity
       activeOpacity={0.7}
-      className="absolute bottom-4 right-4 z-10 rounded-full bg-pink-400 p-4 "
+      className="absolute bottom-4 right-4 z-10 rounded-full bg-pink-400 p-4"
     >
       <AntDesign name="plus" size={24} color="#f9fafb" />
     </TouchableOpacity>
