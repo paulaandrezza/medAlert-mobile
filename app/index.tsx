ec8970714858eb4e9f3a6e358925092d84441a92
paulaandrezza <paulaandrezza25@gmail.com>
2023-06-07 21:43:46 -0300

    atualizando reminderCard

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,13 @@
 import { View } from 'react-native'
 
+import { FabButton } from '../components/FabButton'
 import { ReminderCard } from '../components/ReminderCard'
 
 export default function App() {
   return (
     <View className="mx-4 flex-1 justify-start">
       <ReminderCard />
+      <FabButton />
     </View>
   )
 }
--- components/ReminderCard.tsx
+++ components/ReminderCard.tsx
@@ -1,21 +1,31 @@
-import { AntDesign, MaterialCommunityIcons } from '@expo/vector-icons'
-import { Text, View } from 'react-native'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
+import { useState } from 'react'
+import { Switch, Text, View } from 'react-native'
 import { TouchableOpacity } from 'react-native-gesture-handler'
 
 export function ReminderCard() {
+  const [isTaken, setTaken] = useState(false)
+  const toggleSwitch = () => setTaken(!isTaken)
+
   return (
     <View className="h-fit w-full flex-row items-center justify-between rounded-lg bg-gray-900 px-6 py-4">
       <View className="flex-1 gap-4">
         <Text className="font-body text-pink-400">Margarida</Text>
         <Text className="font-title text-5xl text-gray-50">10:00</Text>
-        <Text className="font-body text-gray-50">Azitromicina</Text>
+        <View>
+          <Text className="font-body text-gray-50">Azitromicina</Text>
+          <Text className="font-body text-gray-50">2 comprimidos</Text>
+        </View>
       </View>
-      <View className="h-full flex-1 items-end gap-5">
-        <TouchableOpacity className="rounded-full bg-gray-700 p-1">
-          <AntDesign name="down" size={20} color="#f9fafb" />
-        </TouchableOpacity>
-        <TouchableOpacity className="rounded-full bg-gray-700 p-3">
-          <MaterialCommunityIcons name="pill" size={32} color="#f9fafb" />
+      <View className="h-full max-h-full items-center justify-between">
+        <Switch
+          trackColor={{ false: '#374151', true: '#f472b6' }}
+          thumbColor={isTaken ? '#f9a8d4' : '#64748b'}
+          onValueChange={toggleSwitch}
+          value={isTaken}
+        ></Switch>
+        <TouchableOpacity className="rounded-full bg-gray-700 p-2">
+          <MaterialCommunityIcons name="pill" size={20} color="#f9fafb" />
         </TouchableOpacity>
       </View>
     </View>
