593d45219cebdef3a83db823cb7cf4e347a736a6
paulaandrezza <paulaandrezza25@gmail.com>
2023-06-08 22:21:06 -0300

    change medicationCard layout

--- components/MedicationCard.tsx
+++ components/MedicationCard.tsx
@@ -1,55 +1,47 @@
-import { MaterialIcons } from '@expo/vector-icons'
-import { useState } from 'react'
-import { Switch, Text, View } from 'react-native'
+import {
+  FontAwesome,
+  Ionicons,
+  MaterialCommunityIcons,
+} from '@expo/vector-icons'
+import { Text, View } from 'react-native'
 import { TouchableOpacity } from 'react-native-gesture-handler'
 
 export function MedicationCard() {
-  const [isActive, setActive] = useState(true)
-  const toggleSwitch = () => setActive(!isActive)
-
-  const [isOpen, setOpen] = useState(false)
-  const toggleArrow = () => setOpen(!isOpen)
-
   return (
-    <View className="mb-2 h-fit w-full rounded-2xl bg-gray-800 px-6 py-4">
-      <View className="flex-row items-center justify-between">
-        <View className="flex-1 gap-4">
-          <Text className="font-body text-pink-400">Margarida</Text>
-          <View>
-            <Text className="font-body text-gray-50">Amoxilina</Text>
-            <View>
-              <Text className="font-body text-gray-50">2 comprimidos</Text>
-            </View>
-            <Text className="font-body text-gray-50">2 vezes ao dia</Text>
-          </View>
-        </View>
-        <View className="h-full max-h-full items-center justify-between">
-          <TouchableOpacity
-            activeOpacity={0.7}
-            className="rounded-full bg-gray-700 p-1"
-            onPress={toggleArrow}
-          >
-            <MaterialIcons
-              name="keyboard-arrow-down"
-              size={20}
-              color="#f9fafb"
-            />
-          </TouchableOpacity>
-          <Switch
-            trackColor={{ false: '#374151', true: '#f472b6' }}
-            thumbColor={isActive ? '#f9a8d4' : '#64748b'}
-            onValueChange={toggleSwitch}
-            value={isActive}
-          ></Switch>
+    <View className="mb-2 grid h-fit w-full grid-cols-1 rounded-2xl bg-gray-800 px-6 py-4">
+      <View className="items-end">
+        <TouchableOpacity activeOpacity={0.7} className="p-2">
+          <FontAwesome name="pencil" size={20} color="#f9fafb" />
+        </TouchableOpacity>
+      </View>
+
+      <View className="mb-4 flex-row items-center gap-x-4">
+        <Ionicons name="person-outline" size={24} color="#f9fafb" />
+        <View>
+          <Text className="font-body text-base text-gray-50">
+            Paula Marinho
+          </Text>
         </View>
       </View>
-      {isOpen && (
+
+      <View className="mb-4 flex-row items-start gap-x-4">
+        <MaterialCommunityIcons name="pill" size={24} color="#f9fafb" />
         <View>
-          <View>
-            <Text className="font-body text-gray-50">Horários</Text>
-          </View>
+          <Text className="font-body text-gray-50">Amoxilina</Text>
+          <Text className="font-body text-gray-50">2 comprimidos</Text>
         </View>
-      )}
+      </View>
+
+      <View className="flex-row items-start gap-x-4">
+        <FontAwesome name="bell-o" size={24} color="#f9fafb" />
+        <View>
+          <Text className="font-body text-gray-50">Horários</Text>
+          <Text className="font-body text-gray-50">04:00</Text>
+          <Text className="font-body text-gray-50">10:00</Text>
+          <Text className="font-body text-gray-50">16:00</Text>
+          <Text className="font-body text-gray-50">22:00</Text>
+        </View>
+      </View>
     </View>
   )
 }
