78efc4d3f9c513d076023a065e78e2f4db2bbf65
paulaandrezza <paulaandrezza25@gmail.com>
2023-07-19 01:42:37 -0300

    starting newMedication

---
+++ app/screens/Medication/NewMedication/TimePicker/index.tsx
@@ -0,0 +1,46 @@
+import DateTimePicker from '@react-native-community/datetimepicker'
+import { useState } from 'react'
+import { Text, TouchableOpacity, View } from 'react-native'
+
+export default function TimePicker() {
+  const [time, setTime] = useState(new Date())
+  const [text, setText] = useState('Selecione um horário')
+  const [show, setShow] = useState(false)
+
+  const onChange = (event, value) => {
+    const currentTime = value || time
+    setShow(false)
+    setTime(currentTime)
+
+    const tempTime = new Date(currentTime)
+    setText(
+      String(tempTime.getHours()).padStart(2, '0') +
+        ':' +
+        String(tempTime.getMinutes()).padStart(2, '0'),
+    )
+  }
+
+  return (
+    <View className="flex-row items-center gap-x-4 pl-10">
+      <TouchableOpacity
+        className="items-left h-12 w-full flex-1 justify-center rounded-md bg-gray-800 px-4 text-gray-50"
+        activeOpacity={0.7}
+        onPress={() => {
+          setShow(true)
+        }}
+      >
+        <Text className="font-body text-gray-50">{text}</Text>
+      </TouchableOpacity>
+
+      {show && (
+        <DateTimePicker
+          testID="dateTimePicker"
+          value={time}
+          mode="time"
+          is24Hour={true}
+          onChange={onChange}
+        />
+      )}
+    </View>
+  )
+}
--- app/screens/Medication/NewMedication/index.tsx
+++ app/screens/Medication/NewMedication/index.tsx
@@ -1,9 +1,80 @@
-import { Text, View } from 'react-native'
+import {
+  FontAwesome,
+  Ionicons,
+  MaterialCommunityIcons,
+} from '@expo/vector-icons'
+import { Picker } from '@react-native-picker/picker'
+import { useState } from 'react'
+import { Text, TouchableOpacity, View } from 'react-native'
+import { TextInput } from 'react-native-paper'
+import TimePicker from './TimePicker'
 
 export default function NewMedication() {
+  const [selectedValue, setSelectedValue] = useState('Selecione um perfil')
+  const [text, onChangeText] = useState('')
+
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-gray-50">New Medication</Text>
+    <View className="mb-2 grid h-fit w-full grid-cols-1 gap-y-3 px-4 py-4">
+      <View className="flex-row items-center gap-x-4">
+        <View className="w-6">
+          <Ionicons name="person" size={24} color="#f9fafb" />
+        </View>
+        <View className="flex-1 rounded-md bg-gray-800">
+          <Picker
+            selectedValue={selectedValue}
+            style={{
+              height: 48,
+              color: 'white',
+            }}
+            dropdownIconColor="white"
+            onValueChange={(itemValue) => setSelectedValue(itemValue)}
+          >
+            <Picker.Item label="Renimar" value="renimar" />
+            <Picker.Item label="Paulinha" value="paulinha" />
+          </Picker>
+        </View>
+      </View>
+
+      <View className="flex-row items-center gap-x-4">
+        <View className="w-6">
+          <MaterialCommunityIcons name="pill" size={24} color="#f9fafb" />
+        </View>
+        <View className="w-full flex-1">
+          <TextInput
+            className="h-12 w-full rounded-t-md bg-gray-800 font-body text-gray-50"
+            onChangeText={onChangeText}
+            value={text}
+            placeholder="Insira o nome da medicação..."
+            underlineColor="#f472b6"
+            activeUnderlineColor="#f472b6"
+          ></TextInput>
+        </View>
+      </View>
+
+      <View className="w-full flex-row items-center gap-x-4">
+        <View className="w-6">
+          <FontAwesome name="bell-o" size={24} color="#f9fafb" />
+        </View>
+        <View className="flex h-12 justify-center">
+          <Text className="font-body text-gray-50">Horários</Text>
+        </View>
+      </View>
+      <TimePicker />
+
+      <View className="flex-row items-center gap-x-4">
+        <View className="w-6">
+          <FontAwesome name="repeat" size={24} color="#f9fafb" />
+        </View>
+        <TouchableOpacity
+          className="items-left h-12 w-full flex-1 justify-center rounded-md bg-gray-800 px-4 text-gray-50"
+          activeOpacity={0.7}
+          onPress={() => {
+            setShow(true)
+          }}
+        >
+          <Text className="font-body text-gray-50">Não repete</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
