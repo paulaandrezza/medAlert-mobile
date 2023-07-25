5fd526d04b394e0816622b9118de01f31d4380e5
paulaandrezza <paulaandrezza25@gmail.com>
2023-07-24 21:23:35 -0300

    removing unit

--- components/DialogCard/Stepper/index.tsx
+++ components/DialogCard/Stepper/index.tsx
@@ -5,20 +5,17 @@ import { Divider, FAB, Text } from 'react-native-paper';
 export default function Stepper({count, setCount}) {
 
   return (
-    <View className='items-center'>
-      <View className='items-center flex-row justify-center py-4'>
-        <FAB
-          icon="minus"
-          onPress={() => setCount(count - 1)}
-        />
-        <Text className='text-lg mx-8'>{count}</Text>
-        <Divider />
-        <FAB
-          icon="plus"
-          onPress={() => setCount(count + 1)}
-        />
-      </View>
-      <Text className='text-lg mx-8'>mililitros</Text>
+    <View className='items-center flex-row justify-center py-4'>
+      <FAB
+        icon="minus"
+        onPress={() => setCount(count - 1)}
+      />
+      <Text className='text-lg mx-8'>{count}</Text>
+      <Divider />
+      <FAB
+        icon="plus"
+        onPress={() => setCount(count + 1)}
+      />
     </View>
   );
 }
